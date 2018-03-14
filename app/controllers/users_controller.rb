eaf7cefc543957f1d9c98afeacdb46f0b6c2f7ca
Nathan <nathan030779@gmail.com>
2018-03-14 21:03:02 +0000

    Fixed some issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,8 +48,9 @@ class UsersController < ApplicationController
   
   def require_same_user
     if current_user != @user
-      flash[:danger] = "You can onlly edit your own account"
+      flash[:danger] = "You can only edit your own account"
       redirect_to root_path
+    end
   end
   
 end
